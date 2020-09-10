205bd244077b833c491112d60ee2224826420b15
Konski <szymon.konski@outlook.com>
2020-09-10 15:45:29 +0200

    Added user class and connection to db

---
+++ ProjectManger/Data/PMContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManger.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManger.Data
+{
+    public class PMContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=ProjectManagerDB;Trusted_Connection=True");
+        }
+        public DbSet<User> User { get; set; }
+
+    }
+}
